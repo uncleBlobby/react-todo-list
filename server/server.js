d624861366ed21b2623fd08883a9eef78305e711
Dustin <dc.christianson@gmail.com>
2022-10-16 18:06:18 -0600

    updated backend routing to have parameterized user and receive todo body

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ const port = 5174;
 app.use(cors());
 app.use(express.json());
 
+const users = [];
+
 app.get('/', (req, res) => {
     console.log(`received request: ${JSON.stringify(req.headers)}`)
-    res.send('Hello, World!')
+    res.json('Hello, World!')
 })
 
 app.get('/login', (req, res) => {
@@ -20,6 +22,30 @@ app.get('/users/:userId/todos', (req, res) => {
     res.send(req.params);
 })
 
+app.post('/users/:userID/todos', (req, res) => {
+    console.log(`request params: ${JSON.stringify(req.params)}`)
+    const user = req.params.userID;
+    if (!users.includes(user)){
+        users.push(user);
+        console.log(`added new user: ${user}`)
+        console.log(`users: ${users}`)
+    }
+    console.log(`received post request at '/users/${user}/todos'`)
+    console.log(`new todo ${JSON.stringify(req.body.todo)}`)
+    
+    //console.log(res.data);
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
+
+/*
+class Todo {
+    constructor(title, description, dueDate, UUID);
+    title = this.title;
+    description = this.description;
+    dueDate = this.dueDate;
+    UUID = this.UUID;
+}
+*/
